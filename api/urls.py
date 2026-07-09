494be8e768af14f349474210f6fde6dab7f76ff1
Tanzeel Ahmed <tanzeelahmed550@gmail.com>
2026-07-09 22:32:40 +0500

    for testing the pipeline

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 from django.http import JsonResponse
 
 def health_check(request):
-    return JsonResponse({"status": "healthy"}, status=200)
+    return JsonResponse({"status": "healthy good night sir you should sleep"}, status=200)
 
 urlpatterns = [
     path('', health_check, name='root_health_check'),
